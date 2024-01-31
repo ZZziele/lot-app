4c503bc370897a328618d63a09963de42d5d6e9d
Uzytkownik <kzielecki@op.pl~>
2024-01-31 17:57:03 +0100

    [add] - enpoint for new planes - in progress

--- src/main/java/com/sda/lot/controller/PlaneRestController.java
+++ src/main/java/com/sda/lot/controller/PlaneRestController.java
@@ -4,9 +4,12 @@ import com.sda.lot.domain.Plane;
 import com.sda.lot.dto.PlaneDto;
 import com.sda.lot.mapper.PlaneMapper;
 import com.sda.lot.service.PlaneService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,10 +38,16 @@ public class PlaneRestController {
                 .map(plane -> planeMapper.fromEntityToDto(plane))
                 .toList();
     }
-    @GetMapping("planes/{id}")
+    @GetMapping("/planes/{id}")
     public PlaneDto getPlaneById(@PathVariable("id") long planeId){
         log.info("Trying to find car with id: [{}]",planeId);
 
     return planeMapper.fromEntityToDto(planeService.findCarById(planeId));
+    }
+
+    @PostMapping("/planes")
+    public void addPlane(@RequestBody @Valid PlaneDto toSave){
+        log.info("adding new Plane: [{}]",toSave);
+
     }
 }
--- src/main/java/com/sda/lot/controller/PlaneRestControllerAdvisor.java
+++ src/main/java/com/sda/lot/controller/PlaneRestControllerAdvisor.java
@@ -4,8 +4,8 @@ import com.sda.lot.dto.ResponseDto;
 import com.sda.lot.excepiton.WrongPlaneIdException;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,9 +20,7 @@ public class PlaneRestControllerAdvisor {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WrongPlaneIdException.class)
     public ResponseDto  handlePlaneNotFoundException(WrongPlaneIdException exc){
-        String path = ServletUriComponentsBuilder.fromCurrentRequest()
-                .build()
-                .getPath();
+        String path = getCurrentRequestPath();
         return ResponseDto.builder()
                 .timestamp(LocalDateTime.now())
                 .status(404)
@@ -32,4 +30,24 @@ public class PlaneRestControllerAdvisor {
                 .path(path)
                 .build();
     }
+
+    private static String getCurrentRequestPath() {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .build()
+                .getPath();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseDto handleBadRequest(MethodArgumentNotValidException exc){
+        
+    return ResponseDto.builder()
+            .timestamp(LocalDateTime.now())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+            .message(exc.getMessage())
+            .path(getCurrentRequestPath())
+            .build();
+    }
 }
+
