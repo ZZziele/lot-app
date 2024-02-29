d203e095705e0586ff78674a3c8a98da8743bd82
Uzytkownik <kzielecki@op.pl~>
2024-02-29 18:42:07 +0100

    [change] - tanslation of domain objects into entities - finished

--- src/main/java/com/sda/lot/domain/PlaneBooking.java
+++ src/main/java/com/sda/lot/domain/PlaneBooking.java
@@ -1,15 +1,40 @@
 package com.sda.lot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "RENTS")
+@Entity
 public class PlaneBooking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @ManyToOne
     Plane bookedPlane;
+    @ManyToOne
     Client client;
-    LocalDate startDate;
-    LocalDate endDate;
+
+
 
     int totalPriceInPolishGrosz;
+
+    @ManyToOne
     Address startLocation;
+    @ManyToOne
     Address endLocation;
 
 
