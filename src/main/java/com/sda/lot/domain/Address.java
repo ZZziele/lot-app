329eda49694a0821deb9acdcd64c207748d396ad
Uzytkownik <kzielecki@op.pl~>
2024-02-29 18:29:23 +0100

    [change] - tanslation of domain objects into entities - in progress

--- src/main/java/com/sda/lot/domain/Address.java
+++ src/main/java/com/sda/lot/domain/Address.java
@@ -1,6 +1,25 @@
 package com.sda.lot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ADDRESSES")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String street;
 
     String zipCode;
--- src/main/java/com/sda/lot/domain/Client.java
+++ src/main/java/com/sda/lot/domain/Client.java
@@ -1,12 +1,28 @@
 package com.sda.lot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "CKUEBTS")
 @Data
     public class Client {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id;
         String name;
         String surname;
         String pesel;
@@ -15,6 +31,7 @@ import java.time.LocalDateTime;
 
         LocalDate dateOfBirth;
         String phone;
+        @ManyToOne
         Address address;
 
         LocalDateTime accountCreated;
